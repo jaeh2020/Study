17c848edf07021c7f3380a69d3137d323f79c274
jaeh <syoto6636@gmail.com>
2022-07-05 23:04:42 +0900

    확인

---
+++ src/main/java/hello/core/member/MemberApp.java
@@ -0,0 +1,14 @@
+package hello.core.member;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
